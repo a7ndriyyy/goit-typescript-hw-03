3883e40a933700c32711e4a23f0584acd1c81e5d
a7ndriyyy <andrii.nastych@gmail.com>
2023-11-22 19:59:47 +0100

    commit

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,44 @@
-const key = new Key();
+class Key {
+  constructor(private signature: number) {}
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+class Person {
+  constructor(private key: Key) {}
+  getKey() {
+    return this.key;
+  }
+}
+abstract class House {
+  door: boolean;
+  tenants: Person[] = [];
+
+  constructor(public key: Key) {
+    this.door = false;
+  }
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Open");
+    } else {
+      console.log("closed");
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("open");
+    } else {
+      console.log("closed");
+    }
+  }
+}
+const key = new Key(Math.floor(Math.random() * 20));
 
 const house = new MyHouse(key);
 const person = new Person(key);
@@ -6,6 +46,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
